13005a2ac46145ec2b80a343221036a46259001d
HP <pavananaik22@gmail.com>
2020-10-20 15:24:15 +0530

    simple file manipulation from count.txt

--- Lab6/index.php
+++ Lab6/index.php
@@ -3,13 +3,12 @@
     <title> Prog 6 </title>
 </head>
 <body>
-<h1> Hello Everyone </h1>
-<h2> x=50 and y=80 </h2>
+<h1> Welcome to my webpage </h1>
 <?php
-$x=50;
-$y=80;
-$res=$x+$y;
-echo "<h2>Result=$res</h2>"
+//file manipulation from count.txt
+$handle=fopen("count.txt","r");
+$str=fread($handle, 10);
+echo "<h2>Hello $str</h2>"
 ?>
 </body>
 </html>
