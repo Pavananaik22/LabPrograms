8b5e1175a6de8dc91a21b37c4d09e9331ef1bad0
HP <pavananaik22@gmail.com>
2020-10-20 16:09:43 +0530

    program to display date and time

--- Lab6/index.php
+++ Lab6/index.php
@@ -6,15 +6,8 @@
 <h1> Welcome to my webpage </h1>
 <?php
 
-$handle=fopen("count.txt","r");
-$count=fread($handle, 10);
-fclose($handle);
-$count=intval($count);
-$count += 1;
-echo "<h2>No of visitors who visited this page : $count </h2>";
-$handle=fopen("count.txt","w");
-fwrite($handle, $count);
-fclose($handle);
+date_default_timezone_set("Asia/Kolkata");
+echo date("d/m/Y H:i:s")
 ?>
 </body>
 </html>
