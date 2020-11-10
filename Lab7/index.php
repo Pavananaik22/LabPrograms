2d8314041c94e17a9035d0bcdbb1bcd82d4cc76c
HP <pavananaik22@gmail.com>
2020-11-10 14:32:00 +0530

    Simple Time

---
+++ Lab7/index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <title> Digital Clock </title>
+    <style>
+        h1{
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+<h1> Digital Clock </h1>
+<hr/>
+<?php
+echo "<h1>".date("h1:i:s A"). "</h1>"
+?>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
